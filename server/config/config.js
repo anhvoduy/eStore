2bb44280277ecc8018d17b3c22cab0a877cc1bf3
duyanh <duyanh2005@gmail.com>
2016-10-09 16:42:57 +0700

    add files

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+var config = {
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'P@ssw0rd',
+    database        : 'ecargo_demo_2016',
+    connectionLimit : 10,
+};
+
+module.exports = config;
