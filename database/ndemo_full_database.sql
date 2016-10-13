ea49f0d300870c4226e3b1930c8f16bb44fe57ff
Anh Vo Duy <anh.voduy@harveynash.vn>
2016-10-13 13:37:26 +0700

    update database

--- database/ndemo_full_database.sql
+++ database/ndemo_full_database.sql
@@ -4,6 +4,31 @@ CREATE SCHEMA `ndemo` ;
 USE `ndemo`;
 
 --
+-- Table structure for table `tblCustomer`
+--
+DROP TABLE IF EXISTS `tblCustomer`;
+CREATE TABLE `tblCustomer` (
+  `CustomerId` int(11) NOT NULL AUTO_INCREMENT,
+  `CustomerNo` varchar(45) NOT NULL,
+  `CustomerName` varchar(45) NOT NULL,
+  `Description` varchar(200) DEFAULT NULL,
+  `Address` varchar(200) DEFAULT NULL,
+  `Mobile` varchar(45) DEFAULT NULL,
+  `Tel` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,  
+  `Deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`CustomerId`),
+  UNIQUE KEY `CustomerId_UNIQUE` (`CustomerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+--
+-- Sample data for table `tblCustomer`
+--
+INSERT INTO `tblCustomer` (`CustomerNo`,`CustomerName`,`Description`,`Deleted`) VALUES (uuid(),'Van Thinh Phat','',0);
+INSERT INTO `tblCustomer` (`CustomerNo`,`CustomerName`,`Description`,`Deleted`) VALUES (uuid(),'Cong ty CP Co Dien Lanh REE','',0);
+INSERT INTO `tblCustomer` (`CustomerNo`,`CustomerName`,`Description`,`Deleted`) VALUES (uuid(),'Cong ty CP FPT','',0);
+
+--
 -- Table structure for table `tblAccount`
 --
 DROP TABLE IF EXISTS `tblAccount`;
