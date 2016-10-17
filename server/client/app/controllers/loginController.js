d2d33ac162623a4fe9e719967b105c263ee3633f
duyanh <duyanh2005@gmail.com>
2016-10-17 22:42:58 +0700

    loginController.js

--- server/client/app/controllers/loginController.js
+++ server/client/app/controllers/loginController.js
@@ -1,18 +1,22 @@
 (function () {
     'use strict';        
     app.controller('LoginController', LoginController);
-    LoginController.$inject = [];        
-	function LoginController() {
-		// models
-		var vm = this;
+    LoginController.$inject = ['$scope', '$rootScope', '$location', 'AuthenticationService'];        
+	function LoginController($scope, $rootScope, $location, AuthenticationService) {
+		// reset login status
+		AuthenticationService.ClearCredentials();
 		
-		
-		// functions
-		function activate() {
-			
-		}
-		
-		/* start */
-		activate();
+		$scope.login = function () {
+			$scope.dataLoading = true;
+			AuthenticationService.Login($scope.username, $scope.password, function (response) {
+				if (response.success) {
+					AuthenticationService.SetCredentials($scope.username, $scope.password);
+					$location.path('/');
+				} else {
+					$scope.error = response.message;
+					$scope.dataLoading = false;
+				}
+			});
+		};
 	}
 })();
