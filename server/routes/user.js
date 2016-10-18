5efff838d3287d2f21d38bb81fb03ea53680ced0
duyanh <duyanh2005@gmail.com>
2016-10-18 08:51:09 +0700

    add some files

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,21 +5,6 @@ var dbContext = require('../config/dbContext');
 var errorHelper = require('../config/errorHelper');
 var userService = require('../services/userService');
 
-/*
-200 OK Standard response for successful HTTP requests
-201 Created Request has been fulfilled.New resource created
-204 No Content Request processed.No content returned
-301 Moved Permanently This and all future requests directed to the given URI
-304 Not Modified Resource has not been modified since last requested
-400 Bad Request Request cannot be fulfilled due to bad syntax
-401 Unauthorized Authentication is possible, but has failed
-403 Forbidden Server refuses to respond to request
-404 Not Found Requested resource could not be found
-500 Internal Server Error Generic error message when server fails
-501 Not Implemented Server does not recognize method or lacks ability to fulfill
-503 Service Unavailable Server is currently unavailable
-*/
-
 // Router
 router.get('/items', function (request, response, next) {
 	var ctx = {};
