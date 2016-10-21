1ee8b38ebd265436f2b7b7a7c9cb494443d64be2
Anh Vo Duy <anh.voduy@harveynash.vn>
2016-10-21 14:36:33 +0700

    optimize file application

--- server/server.js
+++ server/server.js
@@ -6,42 +6,38 @@ var bodyParser = require('body-parser');
 var errorHelper = require('./config/errorHelper');
 
 // Express
-var app = express();
+var server = express();
 var router = express.Router();
-//app.set('port', process.env.PORT || 3000);
-app.set('port', 3000);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//server.set('port', process.env.PORT || 3000);
+server.set('port', 3000);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 /* ----------- Register API -----------*/
-app.use('/api', require('./routes/api'));
-app.use('/api/authenticate', require('./routes/authenticate'));
-app.use('/api/brand', require('./routes/brand'));
-app.use('/api/product', require('./routes/product'));
-app.use('/api/account', require('./routes/account'));
-app.use('/api/user', require('./routes/user'));
-app.use('/api/review', require('./routes/review'));
+server.use('/api', require('./routes/api'));
+server.use('/api/authenticate', require('./routes/authenticate'));
+server.use('/api/brand', require('./routes/brand'));
+server.use('/api/product', require('./routes/product'));
+server.use('/api/account', require('./routes/account'));
+server.use('/api/user', require('./routes/user'));
+server.use('/api/review', require('./routes/review'));
 
 // Error Handling
-app.use(function (error, request, response, next) {
+server.use(function (error, request, response, next) {
 	response.status(500);
 	response.json(errorHelper.errorHandler(error));
 });
 
 
-/* ----------- Register Angular Application -----------*/
-app.use('/app', express.static(path.join(__dirname, 'client/app')));
-app.use('/libs', express.static(path.join(__dirname, 'client/libs')));
+/* ----------- Register Angular serverlication -----------*/
+server.use('/app', express.static(path.join(__dirname, 'client/app')));
+server.use('/libs', express.static(path.join(__dirname, 'client/libs')));
 
 
 // Render page at Client Side
-app.get('/', function (request, response) {
-	response.sendFile(path.join(__dirname + '/client/index.html'));
-	//response.sendFile(path.join(__dirname + '/publish/index.html'));
+server.get('/', function (request, response) {
+	response.sendFile(path.join(__dirname + '/client/index.html'));	
 });
 
-/* ----------- Start Server -----------*/
-http.createServer(app).listen(app.get('port'), function () {
-	console.log('Web is running on port ' + app.get('port'));
-	console.log('API is running on port ' + app.get('port'));
-});
+// export
+module.exports = server;
