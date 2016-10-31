b3cdd33b401f59990340e5be32d8b4a8b235f715
Anh Vo Duy <anh.voduy@harveynash.vn>
2016-10-31 12:32:29 +0700

    add file to components

---
+++ server/client/app/components/account/accountDetailController.js
@@ -0,0 +1,56 @@
+(function (){
+    'use strict';
+	app.controller('accountDetailController', accountDetailController);
+    accountDetailController.$inject = ['$timeout', 'accountService', '$state', '$stateParams'];    
+    function accountDetailController($timeout, accountService, $state, $stateParams) {
+		// models
+		var vm = this;
+        vm.accountId = $stateParams.accountID;		
+		vm.disabledButton = false;
+		vm.messageSuccess = '';
+        vm.messageError = '';
+		vm.save = save;
+		
+		// functions
+		function activate() {
+            accountService.getAccountById(vm.accountId).then(function (result) {
+                vm.account = result;
+				if (vm.account === undefined) {
+                    vm.messageError = String.format("The account: {0} not found.", vm.accountId);
+					vm.disabledButton = true;
+				} else {
+					vm.disabledButton = false;
+				}
+			}, function (error) {
+				vm.messageError = error.message;
+				vm.disabledButton = true;
+			});			
+		}
+		
+		// if update brand success/failed -> reset status after 5s  
+		function resetFormStatus() {
+			$timeout(function () {
+				vm.disabledButton = false;
+				vm.messageSuccess = '';
+				vm.messageError = '';
+			}, 5000);
+		}
+		
+		// buttons
+        function save() {
+            if (vm.account === undefined) return;
+
+            vm.disabledButton = true;
+            accountService.updateAccount(vm.account).then(function (result) {
+                vm.messageSuccess = result.message;
+                resetFormStatus();
+            }, function (error) {
+                vm.messageError = error.message;
+                resetFormStatus();
+            });
+        }
+		
+		/* start */
+		activate();
+	}
+})();
