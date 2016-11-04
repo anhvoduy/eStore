ce45727e800de386d9481b7cddde7f2131849e0e
duyanh <duyanh2005@gmail.com>
2016-11-04 17:57:10 +0700

    add angular.js

--- server/client/libs/angular/angular.js
+++ server/client/libs/angular/angular.js
@@ -29667,8 +29667,7 @@ var ngRepeatDirective = ['$parse', '$animate', '$compile', function($parse, $ani
   var NG_REMOVED = '$$NG_REMOVED';
   var ngRepeatMinErr = minErr('ngRepeat');
 
-  var updateScope = function (scope, index, valueIdentifier, value, keyIdentifier, key, arrayLength) {
-      console.log('---updateScope()..................');
+  var updateScope = function (scope, index, valueIdentifier, value, keyIdentifier, key, arrayLength) {      
     // TODO(perf): generate setters to shave off ~40ms or 1-1.5%
     scope[valueIdentifier] = value;
     if (keyIdentifier) scope[keyIdentifier] = key;
