685eb4f427a1447400bbd5b8ff54ed6b720ed94e
duyanh <duyanh2005@gmail.com>
2016-11-06 12:23:33 +0700

    restructure angular application

--- server/client/app/common/appContext.js
+++ server/client/app/common/appContext.js
@@ -1,15 +1,17 @@
 (function () {
     'use strict';
     app.factory('appContext', appContext);
-    appContext.$inject = ['$http', '$q', '$rootScope', '$modal'];
+    appContext.$inject = ['$http', '$q', '$rootScope', '$modal', '$location', '$cookieStore'];
 
-    function appContext($http, $q, $rootScope, $modal) {
+    function appContext($http, $q, $rootScope, $modal, $location, $cookieStore) {
         // constructor
         var appContext = {
             http: $http,
             rootScope: $rootScope,
             q: $q,
-            modal: $modal
+			modal: $modal,
+			location: $location,
+			cookieStore: $cookieStore
         }		
 		
         return new appContext;
