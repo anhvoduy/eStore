56949263d90e9d535d6e40c7e5e59368c3c072dc
Anh Vo Duy <anh.voduy@harveynash.vn>
2016-11-24 13:53:18 +0700

    add redis cache

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,6 +9,9 @@ var LocalStrategy = require('passport-local').Strategy;
 
 // sample redis cache
 var cache = require('express-redis-cache')();
+var responseTime = require('response-time');
+var axios = require('axios');
+var redis = require('redis');
 
 // our configuration
 var auth = require('../config/auth');
@@ -17,7 +20,6 @@ var constant = require('../config/constant');
 var dbContext = require('../config/dbContext');
 var errorHelper = require('../config/errorHelper');
 var userService = require('../services/userService');
-
 
 
 // routers: use to test
@@ -33,6 +35,65 @@ router.post('/', cache.route({ name: 'post_api' }), function (req, res, next) {
     next();
 });
 
+
+
+// routers TEST REDIS
+// create a new redis client and connect to our local redis instance
+var client = redis.createClient();
+
+// if an error occurs, print it to the console
+client.on('error', function (err) {
+    console.log("Error " + err);
+});
+
+// call the GitHub API to fetch information about the user's repositories
+function getUserRepositories(user) {
+    var githubEndpoint = 'https://api.github.com/users/' + user + '/repos' + '?per_page=100';
+    console.log('-----URL:' + githubEndpoint);
+    return axios.get(githubEndpoint);
+}
+
+// add up all the stars and return the total number of stars across all repositories
+function computeTotalStars(repositories) {
+    return repositories.data.reduce(function (prev, cur) {
+		console.log('- full_name:', cur.full_name);
+		console.log('- stargazers_count:', cur.stargazers_count);
+        return prev + cur.stargazers_count
+    }, 0);
+}
+
+// if a user visits /api/facebook, return the total number of stars 'facebook'
+// has across all it's public repositories on GitHub
+router.get('/:username', function(req, res) {
+	// get the username parameter in the URL
+	// i.e.: username = "coligo-io" in http://localhost:5000/api/coligo-io
+	var username = req.params.username;	
+	console.log('-----username:' + username);
+
+	// use the redis client to get the total number of stars associated to that
+  	// username from our redis cache
+	client.get(username, function(err, result) {
+		if(result){
+			// the result exists in our cache - return it to our user immediately
+        	res.send({ "totalStars": result, "source": "redis cache" });			
+		}else{
+			// we couldn't find the key "coligo-io" in our cache, so get it from the GitHub API
+			getUserRepositories(username).then(computeTotalStars).then(function(totalStars){
+				console.log('- totalStars:', totalStars);
+				// store the key-value pair (username:totalStars) in our cache with an expiry of 1 minute (60s)
+				client.setex(username, 60, totalStars);
+				// return the result to user
+				res.send({ "totalStars": totalStars, "source": "GitHub API" });					
+			}).catch(function(result){
+				if(res.status === 404){
+					res.send('The GitHub username could not be found. Try "coligo-io" as an example!');
+				}else{
+					res.send(result);
+				}
+			});
+		}
+	});
+});
 
 
 
