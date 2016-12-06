80d513a9f6678c0b82ac37939d7a1b4f0400a9f7
Anh Vo Duy <anh.voduy@harveynash.vn>
2016-12-06 17:32:21 +0700

    add crawl schedule.js

--- server/tasks/schedule.js
+++ server/tasks/schedule.js
@@ -1,31 +1,51 @@
 ﻿// Dependencies
-var q = require('q');
+var Q = require('q');
 var axios = require('axios');
 var elasticsearch = require('elasticsearch');
+var dbContext = require('../config/dbContext');
+var productService = require('../services/productService');
 
 // Crawling
-var Crawling = function () {
+var Crawling = function(){
 };
 
-Crawling.prototype.full = function(){
-	console.log('start Crawl Schedule in Full Mode ............');
-};
-
-Crawling.prototype.incremental = function(){
-	console.log('start Crawl Schedule in Incremental Mode ............');
-};
-
-// Schedule
-var schedule = function(){
-	var crawling = new Crawling();
-	crawling.full();
-	crawling.incremental();
-
+Crawling.prototype.connect = Q.async(function*(){
 	var client = new elasticsearch.Client({
 		host: 'localhost:9200',
 		log: 'trace'
 	});
+	return client;
+});
+
+Crawling.prototype.full = Q.async(function*(){
+	console.log('start Crawl Schedule in Full Mode ............');
+	//try{
+	//	var ctx = yield dbContext.getConnection();
+	//	var products = yield productService.getProducts(ctx, 0);
+	//	for(let product of products){
+	//		console.log(product);
+	//	}
+	//}catch(err){
+	//	console.log(err);
+	//	throw err;
+	//}
+});
+
+Crawling.prototype.incremental = Q.async(function*(){
+	console.log('start Crawl Schedule in Incremental Mode ............');
+});
+
+// Schedule
+var main = function* (){
+	var crawling = new Crawling();		
+	try{		
+		yield crawling.connect();
+		yield crawling.full();
+		yield crawling.incremental();
+	}catch(err){
+		console.log(err);
+		throw err;
+	}		
 };
 
-// Export
-module.exports = new schedule;
+Q.spawn(main); //spawn once
