8f63dce9176713d426d48bc8664359579686ace2
duyanh <duyanh2005@gmail.com>
2017-02-10 01:16:35 +0700

    - optimize code Node

--- server/services/brandService.js
+++ server/services/brandService.js
@@ -3,29 +3,29 @@ var Q = require('q');
 var dbHelper = require('../config/dbHelper');
 
 // Constructor
-var brandService = function(){	
+var factory = function(){	
  }
 
-brandService.getBrands = function (ctx) {
+factory.getBrands = function (ctx) {
 	var sql = 'SELECT BrandId, Name, Description FROM tblbrand WHERE deleted = 0 ORDER BY BrandId DESC';
 	return ctx.queryCommand(sql);
 }
 
-brandService.getBrandById = function (ctx, brandId) {
+factory.getBrandById = function (ctx, brandId) {
 	var sql = dbHelper.prepareQueryCommand('SELECT BrandId, Name, Description FROM tblbrand WHERE brandId = ?', [brandId]);
 	return ctx.queryCommand(sql);    
 }
 
-brandService.createBrand = function (ctx, brand) {
+factory.createBrand = function (ctx, brand) {
 	var sql = dbHelper.prepareQueryCommand('', []);
 	return ctx.queryCommand(sql);
 }
 
-brandService.updateBrand = function (ctx, brand) {
+factory.updateBrand = function (ctx, brand) {
 	var sql = dbHelper.prepareQueryCommand('UPDATE tblbrand SET Name = ?, Description = ? WHERE BrandId = ?', 
 		[brand.Name, brand.Description, brand.BrandId]);
 	return ctx.queryCommand(sql);
 }
 
 // Export
-module.exports = brandService;
+module.exports = factory;
--- server/services/customerService.js
+++ server/services/customerService.js
@@ -3,32 +3,32 @@ var Q = require('q');
 var dbHelper = require('../config/dbHelper');
 
 // Constructor
-var customerService = function () { 
+var factory = function () { 
 }
 
-customerService.prototype.getCustomers = function (ctx) {
+factory.prototype.getCustomers = function (ctx) {
 	var sql = dbHelper.prepareQueryCommand('', []);
 	return ctx.queryCommand(sql);
 }
 
-customerService.prototype.getCustomerById = function (ctx, customerId) {
+factory.prototype.getCustomerById = function (ctx, customerId) {
 	var sql = dbHelper.prepareQueryCommand('', [customerId]);
 	return ctx.queryCommand(sql);
 }
 
-customerService.prototype.createCustomer = function (ctx, customer) {
+factory.prototype.createCustomer = function (ctx, customer) {
 	var sql = dbHelper.prepareQueryCommand('', []);
 	return ctx.queryCommand(sql);
 }
 
-customerService.prototype.updateCustomer = function (ctx) {
+factory.prototype.updateCustomer = function (ctx) {
 	var sql = dbHelper.prepareQueryCommand('', []);
 	return ctx.queryCommand(sql);
 }
 
-customerService.prototype.deleteCustomer = function (ctx) {
+factory.prototype.deleteCustomer = function (ctx) {
 	var sql = dbHelper.prepareQueryCommand('', []);
 	return ctx.queryCommand(sql);
 }
 // Export
-module.exports = new customerService;
+module.exports = new factory;
