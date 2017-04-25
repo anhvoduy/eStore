1611fa94ab4bd545b4f13ea7844b488632f534a9
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-04-25 09:25:09 +0700

    - app.js + dev.js

--- server/dev.js
+++ server/dev.js
@@ -2,6 +2,17 @@
 var uuid = require('uuid');
 var http = require('http');
 var server = require('./config/server');
+var memoryMonitor = require('pubnub-rickshaw-memory');
+memoryMonitor.init({dev: true});
+
+memoryMonitor.init({
+  publish_key: 'publishKey',
+  subscribe_key: 'subscribeKey',
+  channel: uuid(),
+  interval_timeout: 1000,
+  dev_mode: false,
+  port: 3333
+});
 
 /* ----------- Start Server -----------*/
 http.createServer(server).listen(server.get('port'), function () {
