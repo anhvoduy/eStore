81cd54dd2d6f0695f92685870424a5104c6d53e9
duyanh <duyanh2005@gmail.com>
2017-06-25 10:40:03 +0700

    revert

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,3 @@
-// https://www.npmjs.com/package/mssql
-
-// SqlServer
-var configMsSql = {
-    user: '...',
-    password: '...',
-    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance 
-    database: '...',
-    options: {
-        encrypt: true // Use this if you're on Windows Azure 
-    }
-}
-
-// MySql
 var configMySql = {
     host: 'localhost',
     user: 'root',
