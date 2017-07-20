7b24f847540f74078db34a9893d15df405da54b4
duyanh <duyanh2005@gmail.com>
2017-07-20 23:16:24 +0700

    react hello world

--- server/publish/app/index.js
+++ server/publish/app/index.js
@@ -1,4 +1,28 @@
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+
+(function(){
+  'use strict';
+  var data = {
+    title: 'Our Short Story',
+    imageUrl: 'images/bg.jpg',
+    desc: 'Lorem ipsum dolor sit amet.',
+    moreInfo: 'More about info ...'
+  } 
+
+  var HeaderArea = React.createClass({
+    getInitialState: function(){
+      return this.props.data;
+    },
+    render: function(){
+      return (
+        <div>
+          <h1>Hello, React!</h1>
+          <h2>{this.state.title}</h2>
+          <p>{this.state.desc}</p>
+          <p>{this.state.desc}</p>
+        </div>        
+      );
+    }
+  });
+
+  ReactDOM.render(<HeaderArea data={data} />, document.getElementById('root'));
+})();
