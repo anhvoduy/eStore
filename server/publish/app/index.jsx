5093c777beeadd0440d0a651c6b54272302ef26a
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-21 14:28:45 +0700

    add files

---
+++ server/publish/app/index.jsx
@@ -0,0 +1,32 @@
+(function(){
+  'use strict';  
+  var HeaderArea = React.createClass({
+    getInitialState: function(){
+      console.log('... getInitialState() ...');
+      return {};
+    },
+    componentWillMount: function(){
+      console.log('... componentWillMount() ...');
+    },
+    componentDidMount: function(){
+      console.log('... componentDidMount() ...');
+      var component = this;      
+      ProfileService.getData().then(function(data){
+        component.setState(data);
+      });
+    },    
+    render: function(){
+      return (
+        <div>
+          <h1>Hello, React!</h1>
+          <p>{this.state.firstName}</p>
+          <p>{this.state.lastName}</p>
+          <p>{this.state.club}</p>
+          <p>{this.state.age}</p>
+        </div>        
+      );
+    }
+  });
+
+  ReactDOM.render(<HeaderArea />, document.getElementById('root'));
+})();
