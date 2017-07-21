9c6616723c0d5b88e2a85e008cc55cccb015ae58
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-21 18:03:17 +0700

    render HeaderArea

--- server/publish/app/index.js
+++ server/publish/app/index.js
@@ -1,6 +1,71 @@
 (function(){
-  'use strict';  
-  var HeaderArea = React.createClass({
+  'use strict';
+  
+  function Welcome (){
+    return (
+      <div>
+        <h1>Hello, React!</h1>
+        <p>{this.state.firstName}</p>
+        <p>{this.state.lastName}</p>
+        <p>{this.state.club}</p>
+        <p>{this.state.age}</p>
+      </div>        
+    );
+  }
+
+  function HeaderArea () {
+    return (
+      <div className="header-area">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="user-menu">
+                        <ul>
+                            <li><a href="#"><i className="fa fa-user"></i> My Account</a></li>
+                            <li><a href="#"><i className="fa fa-heart"></i> Wishlist</a></li>
+                            <li><a href="cart.html"><i className="fa fa-user"></i> My Cart</a></li>
+                            <li><a href="checkout.html"><i className="fa fa-user"></i> Checkout</a></li>
+                            <li><a href="#"><i className="fa fa-user"></i> Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div className="header-right">
+                        <ul className="list-unstyled list-inline">
+                            <li className="dropdown dropdown-small">
+                                <a data-toggle="dropdown" data-hover="dropdown" className="dropdown-toggle" href="#">
+                                  <span className="key">currency :</span>
+                                  <span className="value">USD </span><b className="caret"></b>
+                                </a>
+                                <ul className="dropdown-menu">
+                                  <li><a href="#">USD</a></li>
+                                  <li><a href="#">INR</a></li>
+                                  <li><a href="#">GBP</a></li>
+                                </ul>
+                            </li>
+
+                            <li className="dropdown dropdown-small">
+                                <a data-toggle="dropdown" data-hover="dropdown" className="dropdown-toggle" href="#">
+                                  <span className="key">language :</span>
+                                  <span className="value">English </span><b className="caret"></b>
+                                </a>
+                                <ul className="dropdown-menu">
+                                  <li><a href="#">English</a></li>
+                                  <li><a href="#">French</a></li>
+                                  <li><a href="#">German</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+  };
+
+  var IndexPage = React.createClass({
     getInitialState: function(){
       console.log('... getInitialState() ...');
       return {};
@@ -15,18 +80,8 @@
         component.setState(data);
       });
     },    
-    render: function(){
-      return (
-        <div>
-          <h1>Hello, React!</h1>
-          <p>{this.state.firstName}</p>
-          <p>{this.state.lastName}</p>
-          <p>{this.state.club}</p>
-          <p>{this.state.age}</p>
-        </div>        
-      );
-    }
+    render: HeaderArea //Welcome
   });
 
-  ReactDOM.render(<HeaderArea />, document.getElementById('root'));
+  ReactDOM.render(<IndexPage />, document.getElementById('root'));
 })();
