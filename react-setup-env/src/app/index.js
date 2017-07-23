e9861cbe971a8ec5e7c0005cc844a53aeb0ce389
duyanh <duyanh2005@gmail.com>
2017-07-23 22:47:53 +0700

    multiple components

--- react-setup-env/src/app/index.js
+++ react-setup-env/src/app/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
 class App extends React.Component {
     render() {
         return (
             <div className='container'>
                 <div className='row'>
                     <div className='col-xs-10 col-xs-offset-1'>
-                        <h1>Hello React!</h1>
-                        <p>Setup React Enviroment</p>
+                        <Header />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col-xs-10 col-xs-offset-1'>
+                        <Home />
                     </div>
                 </div>                
             </div>
