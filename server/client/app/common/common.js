eb430125585a3b819a361eac5707439f9bfe8080
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-25 19:14:39 +0700

    remove client folder

--- server/client/app/common/common.js
+++
@@ -1,8 +0,0 @@
-String.format = function() {
-    var s = arguments[0];
-    for (var i = 0; i < arguments.length - 1; i += 1) {
-        var reg = new RegExp('\\{' + i + '\\}', 'gm');
-        s = s.replace(reg, arguments[i + 1]);
-    }
-    return s;
-};
