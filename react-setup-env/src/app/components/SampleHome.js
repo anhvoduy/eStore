fbdc278dc899d5f7fae7925f78fc3ee9e5539d89
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-25 11:32:22 +0700

    update

---
+++ react-setup-env/src/app/components/SampleHome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export class SampleHome extends React.Component{
+    render(){
+        console.log(this.props);
+        return (
+            <div>
+                <p>In a new Component!</p>
+                <p>your name: {this.props.name}, your age: {this.props.age}</p>
+                <p>user object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby) => <li key={hobby.id}>{hobby.name}</li>)}
+                    </ul>
+                </div>
+            </div>            
+        );
+    }
+}
