7def71159916b610973118ceeb7fa60d019b784b
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-07-31 11:23:26 +0700

    update carousel

---
+++ react-setup-env/src/app/components/SampleCarousel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class SampleHome extends React.Component{
+    constructor(props) {
+        super(props);  
+        this.state = {};
+    }
+
+    componentDidMount(){
+    }
+
+    componentWillMount() {        
+    }    
+
+    render(){
+        
+        );
+    }    
+}
