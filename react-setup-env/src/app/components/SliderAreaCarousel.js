3bd2040cf50516ea20bd60ffbea6c2eeba28d460
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-01 18:54:43 +0700

    update position

--- react-setup-env/src/app/components/SliderAreaCarousel.js
+++ react-setup-env/src/app/components/SliderAreaCarousel.js
@@ -9,25 +9,29 @@ export class SliderAreaCarousel extends React.Component {
                 {
                     key: 1,
                     name: 'Slide',
-                    path: 'img/h4-slide.png'
+                    path: 'img/h4-slide.png',
+                    title: 'Iphone'
                 },
                 {
                     key: 2,
                     name: 'Slide',
-                    path: 'img/h4-slide2.png'
+                    path: 'img/h4-slide2.png',
+                    title: 'Sony'
                 },
                 {
                     key: 3,
                     name: 'Slide',
-                    path: 'img/h4-slide3.png'
+                    path: 'img/h4-slide3.png',
+                    title: 'Samsung'
                 },
                 {
                     key: 4,
                     name: 'Slide',
-                    path: 'img/h4-slide4.png'
+                    path: 'img/h4-slide4.png',
+                    title: 'Ipad'
                 }
             ]
-        }        
+        }
     }
     
     componentWillMount() {        
@@ -40,16 +44,29 @@ export class SliderAreaCarousel extends React.Component {
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 2000
+            autoplaySpeed: 200000
         };
         return (
             <Slider {...settings}>
                 {
                     this.state.products.map(function(img)
                     {
-                        return <div><img key={img.key} src={img.path} alt={img.name} /></div>
+                        return (
+                            <div key={img.key} className="block-slider">
+                                <img src={img.path} alt={img.name} />
+                                <div className="caption-group">
+                                    <h2 className="caption title">
+                                        {img.title} <span className="primary">6 <strong>Plus</strong></span>
+                                    </h2>
+                                    <h4 className="caption subtitle">Dual SIM</h4>
+                                    <a className="caption button-radius" href="#">
+                                        <span className="icon"></span>Shop now
+                                    </a>
+                                </div>
+                            </div>
+                        )
                     })
-                }                
+                }
             </Slider>
         );        
     }
