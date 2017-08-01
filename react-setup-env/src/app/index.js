1c9521c7f7c2fc49a601652cf68ae4d9f01c9dfb
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-01 14:24:03 +0700

    update index render

--- react-setup-env/src/app/index.js
+++ react-setup-env/src/app/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Header } from './components/Header';
-import { Home } from './components/Home';
-
 import { HeaderArea } from './components/HeaderArea';
-import { SiteBrandingArea } from './components/SiteBrandingArea';
 import { MainMenuArea } from './components/MainMenuArea';
+import { SiteBrandingArea } from './components/SiteBrandingArea';
+
 import { SliderAreaCarousel } from './components/SliderAreaCarousel';
 import { BrandsAreaCarousel } from './components/BrandsAreaCarousel';
-import { PromoArea } from './components/PromoArea';
+
+//import { PromoArea } from './components/PromoArea';
 import { FooterTopArea } from './components/FooterTopArea';
 import { FooterBottomArea } from './components/FooterBottomArea';
 
@@ -19,7 +18,7 @@ ReactDOM.render(<HeaderArea/>, document.getElementById('HeaderArea'));
 ReactDOM.render(<MainMenuArea/>, document.getElementById('MainMenuArea'));
 ReactDOM.render(<SiteBrandingArea/>, document.getElementById('SiteBrandingArea'));
 
-//ReactDOM.render(<SliderAreaCarousel/>, document.getElementById('SliderAreaCarousel'));
+ReactDOM.render(<SliderAreaCarousel/>, document.getElementById('SliderAreaCarousel'));
 ReactDOM.render(<BrandsAreaCarousel/>, document.getElementById('BrandsAreaCarousel'));
 
 ReactDOM.render(<FooterTopArea/>, document.getElementById('FooterTopArea'));
