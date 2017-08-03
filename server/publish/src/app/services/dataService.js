a6b03e24c857e7dcff0492eca57b257c1d408798
duyanh <duyanh2005@gmail.com>
2017-08-04 02:06:54 +0700

    update dataService.js using fetch api

--- server/publish/src/app/services/dataService.js
+++ server/publish/src/app/services/dataService.js
@@ -1,46 +1,60 @@
-var $ = require('jquery');
+import React from 'react';
 
-module.exports = {
-    get: function(url){
-        return new Promise(function(success, error){
-            $.ajax({
-                url:url,
-                dataType:'json',
-                success: success,
-                error: error
-            })
-        })
-    },
-    post: function(url, data){
-        return new Promise(function(success, error){
-            $.ajax({
-                url:url,
-                type:'POST',
-                data: data,
-                success: success,
-                error: error
-            })
-        })
-    },
-    patch: function(url, data){
-        return new Promise(function(success, error){
-            $.ajax({
-                url:url,
-                type:'PATCH',
-                data: data,
-                success: success,
-                error: error
-            })
-        })
-    },
-    del: function(url){
-        return new Promise(function(success, error){
-            $.ajax({
-                url:url,
-                type:'DELETE',                
-                success: success,
-                error: error
-            })
-        })
-    }
+var dataService ={};
+
+dataService.getProfile = function(){
+    return fetch('http://localhost:3000/api/myprofile').then( function(response) {
+        return response.json();
+    })
 }
+
+module.exports = dataService;
+
+
+
+// var $ = require('jquery');
+
+// module.exports = {
+//     get: function(url){
+//         return new Promise(function(success, error){
+//             $.ajax({
+//                 url:url,
+//                 dataType:'json',
+//                 success: success,
+//                 error: error
+//             })
+//         })
+//     },
+//     post: function(url, data){
+//         return new Promise(function(success, error){
+//             $.ajax({
+//                 url:url,
+//                 type:'POST',
+//                 data: data,
+//                 success: success,
+//                 error: error
+//             })
+//         })
+//     },
+//     patch: function(url, data){
+//         return new Promise(function(success, error){
+//             $.ajax({
+//                 url:url,
+//                 type:'PATCH',
+//                 data: data,
+//                 success: success,
+//                 error: error
+//             })
+//         })
+//     },
+//     del: function(url){
+//         return new Promise(function(success, error){
+//             $.ajax({
+//                 url:url,
+//                 type:'DELETE',                
+//                 success: success,
+//                 error: error
+//             })
+//         })
+//     }
+// }
