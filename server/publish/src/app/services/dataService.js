a88f9aed2e8577b8ed4a46fc32fabdf91a9b0b2b
duyanh <duyanh2005@gmail.com>
2017-08-05 20:49:58 +0700

    use fetch api

--- server/publish/src/app/services/dataService.js
+++ server/publish/src/app/services/dataService.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 var dataService ={};
+dataService.hostName = 'http://localhost:3000';
 
-var hostName = 'http://localhost:3000';
 dataService.getProfile = function(url){
-    return fetch(hostName + url).then( function(response) {
+    return fetch(this.hostName + url).then( function(response) {
         return response.json();
     })
+}
+
+dataService.get = function(url){
+    return fetch(this.hostName + url)
+    .then(function(res){
+        return res.json();
+    });
+}
+
+dataService.post = function(url, data){
+    return fetch(this.hostName + url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+    .then(function(res){
+        return res.json();
+    });
 }
 
 module.exports = dataService;
