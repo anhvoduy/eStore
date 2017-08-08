ca572de88d2f203a6aedca28704c87dfd30d1df2
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-08 14:07:56 +0700

    update productWidgetArea

--- server/publish/src/app/index.js
+++ server/publish/src/app/index.js
@@ -7,7 +7,7 @@ import { SiteBrandingArea } from './components/SiteBrandingArea';
 import { PromoArea } from './components/PromoArea';
 import { SliderAreaCarousel } from './components/SliderAreaCarousel';
 import { BrandsAreaCarousel } from './components/BrandsAreaCarousel';
-
+import { ProductWidgetAreaZone } from './components/ProductWidgetArea'; 
 import { FooterTopArea } from './components/FooterTopArea';
 import { FooterBottomArea } from './components/FooterBottomArea';
 
@@ -19,6 +19,6 @@ ReactDOM.render(<SiteBrandingArea/>, document.getElementById('SiteBrandingArea')
 ReactDOM.render(<PromoArea/>, document.getElementById('PromoArea'));
 ReactDOM.render(<SliderAreaCarousel/>, document.getElementById('SliderAreaCarousel'));
 ReactDOM.render(<BrandsAreaCarousel/>, document.getElementById('BrandsAreaCarousel'));
-
+ReactDOM.render(<ProductWidgetAreaZone/>, document.getElementById('ProductWidgetAreaZone'));
 ReactDOM.render(<FooterTopArea/>, document.getElementById('FooterTopArea'));
 ReactDOM.render(<FooterBottomArea/>, document.getElementById('FooterBottomArea'));
