d3191d2d43e59f906ef52c2026ecf2b4d612b77c
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-14 14:27:58 +0700

    add truckService.js

---
+++ server/lib/constant.js
@@ -0,0 +1,54 @@
+﻿/*
+200 OK Standard response for successful HTTP requests
+201 Created Request has been fulfilled.New resource created
+204 No Content Request processed.No content returned
+301 Moved Permanently This and all future requests directed to the given URI
+304 Not Modified Resource has not been modified since last requested
+400 Bad Request Request cannot be fulfilled due to bad syntax
+401 Unauthorized Authentication is possible, but has failed
+403 Forbidden Server refuses to respond to request
+404 Not Found Requested resource could not be found
+500 Internal Server Error Generic error message when server fails
+501 Not Implemented Server does not recognize method or lacks ability to fulfill
+503 Service Unavailable Server is currently unavailable
+*/
+
+// Constructor
+var constant = function () { 
+}
+
+constant.transactionType = {
+	cashIn: 'CASHIN',
+	cashOut: 'CASHOUT',
+	STOCKIN: 'STOCKIN',
+	STOCKOUT: 'STOCKOUT',
+	journal: 'JOURNAL',
+};
+
+// Success Lists
+constant.Success_Authentication = { code: 'SUCCESS_Authentication', message: 'Authentication is success.' };
+constant.Success_Login = { code: 'SUCCESS_LOGIN', message: 'Login is success.' };
+constant.Success_Logout = { code: 'SUCCESS_LOGOUT', message: 'Logout is success.' };
+
+constant.Success_Cash_Create = { code: 'SUCCESS_CASH_CREATE', message: 'Create Cash is success.' };
+constant.Success_Cash_Update = { code: 'SUCCESS_CASH_UPDATE', message: 'Update Cash is success.' };
+constant.Success_Cash_Delete = { code: 'SUCCESS_CASH_DELETE', message: 'Delete Cash is success.' };
+
+constant.Success_Stock_Create = { code: 'SUCCESS_STOCK_CREATE', message: 'Create Stock is success.' };
+constant.Success_Stock_Update = { code: 'SUCCESS_STOCK_UPDATE', message: 'Update Stock is success.' };
+constant.Success_Stock_Delete = { code: 'SUCCESS_STOCK_DELETE', message: 'Delete Stock is success.' };
+
+constant.Success_Brand_Create = { code: 'SUCCESS_BRAND_CREATE', message: 'Create Brand is success.' };
+constant.Success_Brand_Update = { code: 'SUCCESS_BRAND_UPDATE', message: 'Update Brand is success.' };
+constant.Success_Brand_Delete = { code: 'SUCCESS_BRAND_DELETE', message: 'Delete Brand is success.' };
+
+constant.Success_Product_Create = { code: 'SUCCESS_PRODUCT_CREATE', message: 'Create Product is success.' };
+constant.Success_Product_Update = { code: 'SUCCESS_PRODUCT_UPDATE', message: 'Update Product is success.' };
+constant.Success_Product_Delete = { code: 'SUCCESS_PRODUCT_DELETE', message: 'Delete Product is success.' };
+
+constant.Success_User_Create = { code: 'SUCCESS_USER_CREATE', message: 'Create User is success.' };
+constant.Success_User_Update = { code: 'SUCCESS_USER_UPDATE', message: 'Update User is success.' };
+constant.Success_User_Delete = { code: 'SUCCESS_USER_DELETE', message: 'Delete User is success.' };
+
+// Export
+module.exports = constant;
---
+++ server/services/truckService.js
@@ -0,0 +1,59 @@
+﻿const _ = require('lodash');
+const Q = require('q');
+const dbContext = require('../lib/dbContext');
+const data = require('../database/sampleData');
+
+// Constructor
+const Factory = function () { 
+}
+
+Factory.prototype.getTrucks = function(){
+    let deferred  = Q.defer();
+    let trucks;
+
+    Q.when()
+    .then(function(){
+        return dbContext.openConnection();
+    })
+    .then(function(pool){
+        let sql = `
+            SELECT TruckId, TruckName, TruckNumber, Description
+            FROM Truck
+            ORDER BY TruckId DESC
+        `;
+        return dbContext.queryDatabase(pool, sql)
+		.then(function(data){
+			trucks = data;
+        })
+        .then(function(){
+            dbContext.closeConnection(pool);
+        });
+    })    
+    .then(function(){
+        deferred.resolve(trucks);
+    })
+    .catch(function(err){
+        deferred.reject(err);
+    });
+
+    return deferred.promise;
+}
+
+Factory.prototype.getTruckById = function(){
+    return data.getUser();
+}
+
+Factory.prototype.createTruck = function(){
+    return data.getUsers();
+}
+
+Factory.prototype.updateTruck = function(){
+    return data.getUsers();
+}
+
+Factory.prototype.deleteTruck = function(){
+    return data.getMenus();
+}
+
+// Export
+module.exports = new Factory;
