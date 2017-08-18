c0d17af4aef389acf6ae795346941b8c3e01151d
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-18 14:56:42 +0700

    update checkout: html + js

--- server/publish/src/app/checkout.js
+++ server/publish/src/app/checkout.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HeaderArea } from './components/HeaderArea';
+import { SiteBrandingArea } from './components/SiteBrandingArea';
+import { ProductBigTitleArea } from './components/ProductBigTitleArea';
 import { FooterTopArea } from './components/FooterTopArea';
 import { FooterBottomArea } from './components/FooterBottomArea';
 
 ReactDOM.render(<HeaderArea/>, document.getElementById('HeaderArea'));
+ReactDOM.render(<SiteBrandingArea/>, document.getElementById('SiteBrandingArea'));
+ReactDOM.render(<ProductBigTitleArea/>, document.getElementById('ProductBigTitleArea'));
 ReactDOM.render(<FooterTopArea/>, document.getElementById('FooterTopArea'));
 ReactDOM.render(<FooterBottomArea/>, document.getElementById('FooterBottomArea'));
