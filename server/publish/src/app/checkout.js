f918a4719b1a38a84dd90bd7e2bc386f3d9d4c18
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-18 15:30:13 +0700

    update checkout content

--- server/publish/src/app/checkout.js
+++ server/publish/src/app/checkout.js
@@ -5,6 +5,7 @@ import { HeaderArea } from './components/HeaderArea';
 import { SiteBrandingArea } from './components/SiteBrandingArea';
 import { ProductBigTitleArea } from './components/ProductBigTitleArea';
 import { SingleSideBarZone } from './components/SingleSideBarZone';
+import { ProductContentAreaCheckout } from './components/ProductContentAreaCheckout';
 import { FooterTopArea } from './components/FooterTopArea';
 import { FooterBottomArea } from './components/FooterBottomArea';
 
@@ -12,5 +13,6 @@ ReactDOM.render(<HeaderArea/>, document.getElementById('HeaderArea'));
 ReactDOM.render(<SiteBrandingArea/>, document.getElementById('SiteBrandingArea'));
 ReactDOM.render(<ProductBigTitleArea/>, document.getElementById('ProductBigTitleArea'));
 ReactDOM.render(<SingleSideBarZone/>, document.getElementById('SingleSideBarZone'));
+ReactDOM.render(<ProductContentAreaCheckout/>, document.getElementById('ProductContentAreaCheckout'));
 ReactDOM.render(<FooterTopArea/>, document.getElementById('FooterTopArea'));
 ReactDOM.render(<FooterBottomArea/>, document.getElementById('FooterBottomArea'));
