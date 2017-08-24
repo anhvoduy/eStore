aeeb71a4198092f36020f8a55487f2e38a5d7d24
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-08-24 20:50:23 +0700

    update ReviewForm.js

--- server/publish/src/app/components/ProductContentRightSingle.js
+++ server/publish/src/app/components/ProductContentRightSingle.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReviewForm } from './ReviewForm';
 import reviewService from './../services/reviewService';
 
 export class ProductContentRightSingle extends React.Component {
@@ -7,13 +8,7 @@ export class ProductContentRightSingle extends React.Component {
 
         // https://facebook.github.io/react/docs/forms.html
         this.state = {
-            value: '',
-
-            rating: 1,
-            comment: '',
-            productId: 0,
-            email: '',
-            name: ''            
+            value: ''        
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);        
@@ -196,97 +191,3 @@ export class ProductContentRightSingle extends React.Component {
         );
     }
 };
-
-class ReviewForm extends React.Component {
-    constructor(){
-        super();        
-        this.state = {
-            rating: 1,
-            comment: '',
-            productId: 1,
-            email: '',
-            name: ''            
-        };
-        this.onChangeName = this.onChangeName.bind(this);
-        this.resetReview = this.resetReview.bind(this);
-    }
-
-    resetReview(){
-        //var self = this
-        this.setState({
-            rating: 1,
-            comment: '',
-            productId: 1,
-            email: '',
-            name: ''            
-        });
-    }
-
-    handleSubmit(event) {        
-        event.preventDefault();
-        var self = this;
-        return reviewService.createReview(this.state)
-        .then(function(res){
-            if(res.status) {                
-                console.log('success');
-                self.resetReview();
-            }
-            else {
-                console.log('failed');
-            }
-        });
-    }
-
-    onChangeName(event){
-        let name = event.target.name;
-        let value = event.target.value;
-        this.setState({
-            [name]: value
-        });
-    }    
-
-    render() {
-        return  (
-            <div>
-                <h2>Reviews</h2>
-                <div className="submit-review">
-                    <p>
-                        <label htmlFor="name">Name</label> 
-                        <input name="name" 
-                               type="text"
-                               value={this.state.name}
-                               onChange={this.onChangeName.bind(this)} />
-                    </p>
-                    <p>
-                        <label htmlFor="email">Email</label> 
-                        <input name="email" 
-                               type="email" 
-                               value={this.state.email}
-                               onChange={this.onChangeName.bind(this)} />
-                    </p>
-                    <div className="rating-chooser">
-                        <p>Your rating</p>
-                        <div className="rating-wrap-post">
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                        </div>
-                    </div>
-                    <p>
-                        <label htmlFor="comment">Your review</label> 
-                        <textarea name="comment" 
-                                  cols="30" 
-                                  rows="10" 
-                                  value={this.state.comment}
-                                  onChange={this.onChangeName.bind(this)} />
-                    </p>
-                    <p>
-                        <input type="submit" value="Submit" onClick={this.handleSubmit.bind(this)}/>
-                    </p>
-                </div>
-            </div>
-        )
-    }
-}
