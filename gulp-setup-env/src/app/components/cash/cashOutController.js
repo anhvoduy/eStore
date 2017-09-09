72a90dece3927d72b86938ef38f774490ae39478
duyanh <duyanh2005@gmail.com>
2017-09-09 20:10:02 +0700

    move files to gulp build process

---
+++ gulp-setup-env/src/app/components/cash/cashOutController.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+	app.controller('cashOutController', cashOutController);
+	cashOutController.$inject = ['cashService'];
+	function cashOutController(cashService) {
+		// models
+		var vm = this;
+		vm.messageError = '';
+		vm.messageSuccess = '';
+
+		// functions
+		var activate = function () {
+			cashService.getCashOut().then(function (result) {
+				vm.transactions = result;
+			}, function (error) {
+				vm.messageError = error.message;
+			});
+		};
+		
+		/* start */
+		activate();
+	}
+})();
---
+++ gulp-setup-env/src/app/controllers/baseController.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';        
+    app.controller('baseController', baseController);
+	baseController.$inject = ['$scope', '$rootScope'];
+
+	function baseController($scope, $rootScope) {
+		// declare models
+		var vm = this;		
+		
+		// declare functions
+		function activate() {
+			
+        }
+
+        // activate
+        activate();
+	}
+})();
