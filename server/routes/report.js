d34f8e3c404aaccd032786c99e3eaaf76a38d3e7
anhvodhvn <anh.voduy@harveynash.vn>
2017-10-28 17:40:59 +0700

    clean up

--- server/routes/report.js
+++ server/routes/report.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Q = require('q');
-//const auth = require('../config/auth');
+const auth = require('../config/auth');
 const CONSTANT = require('../lib/constant');
 const errorHelper = require('../lib/errorHelper');
 
-// Router
-router.get('/reportCashIn', function (req, res, next) {
+router.get('/cash', function (req, res, next) {
 	res.status(200).json({ code: 'OK', message: "Success." });
 });
 
-router.get('/reportCashOut', function (req, res, next) {
+router.get('/inventory', function (req, res, next) {
     res.status(200).json({ code: 'OK', message: "Success." });
 });
 
-router.get('/cashBalance', function (req, res, next) {
-    res.status(200).json({ code: 'OK', message: "Success." });
-});
-
-router.get('/inventoryBalance', function (req, res, next) {
-    res.status(200).json({ code: 'OK', message: "Success." });
-});
-
-// return Router
 module.exports = router;
