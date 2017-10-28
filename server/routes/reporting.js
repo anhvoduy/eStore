1ea444c96731f593e151c3e82e534f9e497b66a4
anhvodhvn <anh.voduy@harveynash.vn>
2017-10-28 08:52:50 +0700

    clean up server.js

--- server/routes/reporting.js
+++ server/routes/reporting.js
@@ -1,4 +1,3 @@
-// Dependencies
 const express = require('express');
 const router = express.Router();
 const q = require('q');
@@ -6,15 +5,10 @@ const auth = require('../config/auth');
 const constant = require('../lib/constant');
 const dbContext = require('../lib/dbContext');
 const errorHelper = require('../lib/errorHelper');
-const brandService = require('../services/brandService');
 
 // Router
-router.get('/items', function (request, response, next) {
-	
-});
-
-router.get('/items/:id', function (request, response, next) {
-    
+router.get('/getdata', function (req, res, next) {
+    res.status(200).json(true);
 });
 
 // return Router
