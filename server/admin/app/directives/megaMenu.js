f6ffe68e82fe57067c5044972af39a06699b57e3
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-10-31 17:16:13 +0700

    update megaMenu.js & megaMenu.tpl.html

--- server/admin/app/directives/megaMenu.js
+++ server/admin/app/directives/megaMenu.js
@@ -1,14 +1,17 @@
 (function () {
-    angular.module('cargo.directives.megaMenu', []).directive('megaMenu', function () {
+    angular.module('cargo.directives.megaMenu', []).directive('megaMenu',['$rootScope', function ($rootScope) {
+        var link = function (scope, element, attrs, ngCtrl) {
+            // TO DO: review to hide mega menu at login view
+            console.log($rootScope.hideMegaMenu);
+        }
+
         return {
             restrict: 'EA',
             replace: true,
             templateUrl: function () {
                 return "/app/directives/megaMenu.tpl.html";
             },
-            link: function (scope, element, attrs, ngCtrl) {
-				console.log('init mega-menu');
-            }
+            link: link
         };
-    });
+    }]);
 })();
