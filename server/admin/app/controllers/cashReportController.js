57492ffe78fe930791c90260fd3231a6d5db83e8
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-10-31 21:22:30 +0700

    update cash report

--- server/admin/app/controllers/cashReportController.js
+++ server/admin/app/controllers/cashReportController.js
@@ -8,9 +8,19 @@
 		vm.messageError = '';
 		vm.messageSuccess = '';
 
+		vm.currentDate = new Date();
+		vm.query = {
+			FromDate: new Date(vm.currentDate.getFullYear(), vm.currentDate.getMonth(), 1),
+			ToDate: new Date(vm.currentDate.getFullYear(), vm.currentDate.getMonth() + 1, 0)
+		}
+
 		// functions
 		var activate = function () {			
 		};
+
+		vm.queryReport = function(query){
+			console.log(query);
+		}
 		
 		/* start */
 		activate();
