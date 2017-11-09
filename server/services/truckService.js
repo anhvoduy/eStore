33f55bd07d4eb3b019bf9102816fb4f87edc17be
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-11-09 10:40:48 +0700

    clean up

--- server/services/truckService.js
+++ server/services/truckService.js
@@ -1,5 +1,5 @@
-﻿const _ = require('lodash');
-const Q = require('q');
+﻿const Q = require('q');
+const _ = require('lodash');
 const dbContext = require('../lib/dbContext');
 const data = require('../database/sampleData'); // TO DO: remove
 
