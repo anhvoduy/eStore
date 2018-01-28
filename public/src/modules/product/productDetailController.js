51c6feb573ed73491cddd4b5b568c1baeff267e8
anhvodhvn <anh.voduy@harveynash.vn>
2018-01-28 21:31:05 +0700

    - pass parameter ProductId

--- public/src/modules/product/productDetailController.js
+++ public/src/modules/product/productDetailController.js
@@ -1,24 +1,48 @@
 (function () {
     'use strict';        
-    angular.module('product.detail.controller', ['product.service']).controller('productDetailController', productDetailController);
-	productDetailController.$inject = ['$q','productService'];
-	function productDetailController($q, productService) {
+	angular.module('product.detail.controller', ['product.service']).controller('productDetailController', productDetailController);
+
+	productDetailController.$inject = ['$location', '$q','productService'];
+	function productDetailController($location, $q, productService) {
 		/* view-model */
 		var vm = this;
 		var productService = new productService();		
 		
 		/* functions */
-		function activate() {
+		function activate() {			
+			var productId = getProductId($location.$$absUrl);
+			console.log('- productId:', productId);
 			return productService.getProducts().then(function(result){
 				if(result){
 					vm.products = result.PageData;
 					vm.hello = 'hello product';
 				}
-				console.log('productDetailController ....');
+				console.log('productDetailController ....');				
 			}, function(err){
 				console.log(err);
 			});
 		};
+
+
+		function getProductId(url) {
+			var array = url.split('/');
+			var params = filterArray(array);
+			return params[params.length-1];
+		}
+
+		function filterArray(array) {
+			var index = -1,
+				arr_length = array ? array.length : 0,
+				resIndex = -1,
+				result = [];	
+			while (++index < arr_length) {
+				var value = array[index];		
+				if (value) {
+					result[++resIndex] = value;
+				}
+			}		
+			return result;
+		}		
 		
 		/* start */
 		activate();
