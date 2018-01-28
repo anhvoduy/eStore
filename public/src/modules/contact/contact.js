f88bf4c6a69785ea0d81971fe6c5c25e91b96a1e
anhvodhvn <anh.voduy@harveynash.vn>
2018-01-28 20:30:15 +0700

    clean up module Contact

--- public/src/modules/contact/contact.js
+++ public/src/modules/contact/contact.js
@@ -1,5 +1,5 @@
 angular.module('contact', [
 	'contact.controller',
-	//'contact.directive',
+	'contact.directive',
 	'contact.service'
 ]);
--- server/server.js
+++ server/server.js
@@ -70,6 +70,7 @@ server.use(function (err, req, res, next) {
 var pathPublic = path.join(__dirname, '../public/src/');
 server.use('/', express.static(pathPublic, { index: 'index.html' }));
 server.use('/index', express.static(pathPublic, { index: 'index.html' }));
+server.use('/product', express.static(pathPublic, { index: 'product.html' }));
 server.use('/contact', express.static(pathPublic, { index: 'contact.html' }));
 
 var pathAdmin = path.join(__dirname, '../client/src/');
