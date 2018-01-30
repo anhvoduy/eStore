6db90bbf2c29540e86d48ec07be06c94a6a3153e
Anh Vo Duy <anh.voduy@harveynash.vn>
2018-01-30 11:50:45 +0700

    - fixed unit test

---
+++ server/test/testBrand.js
@@ -0,0 +1,89 @@
+﻿'use strict';
+var chai = require('chai');
+//var chaiHttp = require('chai-http');
+var expect  = require("chai").expect;
+var supertest = require("supertest");
+var server = require('../server');
+//chai.use(chaiHttp);
+
+var brandId = 10;
+var brand = {
+    BrandId: 10,
+    BrandName: 'Sony Vaio',
+    Description: 'Sony Vaio'
+};
+
+describe("GET API: /api/brand/", function() {
+    beforeEach(function() {
+    });
+
+    afterEach(function() {
+    });
+
+    it("Get List Brands", function (done) {
+        supertest(server)
+			.get("/api/brand/items")
+			.expect(200)
+			.end(function (err, res) {
+            
+            if (err) {
+                return done(err);
+            }            
+            expect(res.status, 'Status').eql(200);
+            expect(res.body, 'Body').all.keys('HitsTotal', 'PageTotal', 'PageSize', 'PageCurrent', 'PageData');            
+            res.body.PageData.forEach(function (brand) {
+                expect(brand).all.keys('BrandId', 'BrandKey', 'BrandName', 'Description');
+                expect(brand.BrandId,'BrandId').a('number');
+                expect(brand.BrandKey,'BrandKey').a('string');
+                expect(brand.BrandName,'BrandName').a('string');
+                expect(brand.Description,'Description').a('string');
+            });
+            done();
+        })
+    });
+		
+    it("Get Brand with BrandId: " + brandId, function (done) {
+        supertest(server)
+            .get("/api/brand/item")
+            .query({BrandId: brandId})
+			.expect(200)
+			.end(function (err, res) {
+            
+            if (err) {
+                return done(err);
+            }
+            expect(res.status, 'Status').eql(200);
+            expect(res.body).all.keys('BrandId', 'BrandKey', 'BrandName', 'Description');
+            expect(res.body.BrandId,'BrandId').a('number');
+            expect(res.body.BrandKey,'BrandKey').a('string');
+            expect(res.body.BrandName,'BrandName').a('string');
+            expect(res.body.Description,'Description').a('string');
+            done();
+        })
+    });
+});
+
+describe("POST API: /api/brand/", function () {
+    beforeEach(function() {
+    });
+
+    afterEach(function() {
+    });
+
+    it('POST: update Brand with BrandId = 10', function (done) {
+        supertest(server)
+            .post("/api/brand/update")
+            .send(brand)
+            .expect(200)
+            .end(function (err, res) {
+            
+            if (err) {
+                return done(err);
+            }
+
+            expect(res.status, 'Status').eql(200);
+            expect(res.body.success, 'success').eq(true);
+            done();            
+        });
+    });
+});
---
+++ server/test/testProduct.js
@@ -0,0 +1,105 @@
+﻿'use strict';
+var expect  = require("chai").expect;
+var supertest = require("supertest");
+var server = 'http://localhost:8000';
+
+describe("GET API: /api/product/", function () {
+    var pageIndex = 0;
+    it("Get List Products with Page Index = " + pageIndex, function (done) {
+        supertest(server)
+			.get("/api/product/itemspaging/" + pageIndex)
+			.expect(200)
+			.end(function (error, response) {
+            
+            if (error) {
+                return done(error);
+            }
+            
+            response.body.forEach(function (product) {
+                expect(product).property('ProductId');
+                expect(product).property('ProductName');
+                expect(product).property('Description');
+                expect(product).property('Price');
+                expect(product).property('Colour');
+                expect(product).property('Status');
+                
+                expect(product.ProductId).a('number');
+                expect(product.ProductName).a('string');
+                expect(product.Description).a('string');
+                expect(product.Price).a('number');
+                expect(product.Colour).a('string');
+                expect(product.Status).a('string');
+            });
+            done();
+        })
+    });
+    
+    var productId = 2;
+    it("Get Product with ProductId = " + productId, function (done) {
+        supertest(server)
+			.get("/api/product/items/" + productId)
+			.expect(200)
+			.end(function (error, response) {
+            
+            if (error) {
+                return done(error);
+            }
+            
+            var product = response.body;
+            expect(product).property('ProductId');
+            expect(product).property('ProductName');
+            expect(product).property('Description');
+            expect(product).property('BrandId');
+            expect(product).property('BrandName');
+            expect(product).property('Price');
+            expect(product).property('Colour');
+            expect(product).property('Status');
+            
+            expect(product.ProductId).a('number');
+            expect(product.ProductName).a('string');
+            expect(product.Description).a('string');
+            expect(product.BrandId).a('number');
+            expect(product.BrandName).a('string');
+            expect(product.Price).a('number');
+            expect(product.Colour).a('string');
+            expect(product.Status).a('string');
+            done();
+        })
+    });
+});
+
+
+describe("POST API: /api/review/", function () {
+    var review = {
+        Rating: 2,
+        Comment: 'Test Rating 2xx - Product Id = 2',
+        Created: '',
+        ProductId: 2,
+        Email: 'anhvod@hvn.com',						
+    };
+    // valid Email  :  anhvod@hvn.com
+    // invalid Email:  anhvod@csc.com
+    
+    it('post /api/review/add', function (done) {
+        supertest(server)
+            .post("/api/review/add")
+            .send(review)
+            .expect(201)
+            .end(function (error, response) {
+            
+            if (error) {
+                return done(error);
+            }
+            
+            var result = response.body;            
+            expect(result).property('code');
+            expect(result).property('message');
+                        
+            expect(result.code).a('string');
+            expect(result.message).a('string');
+			
+			expect(result.code).to.equal('CREATE_REVIEW_SUCCESS');
+			done();            
+        });
+    });
+});
