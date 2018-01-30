0538b741f43deb9a1e09f8eb30b190fb5fac3a7b
Anh Vo Duy <anh.voduy@harveynash.vn>
2018-01-30 16:00:33 +0700

    format

--- server/test/testSample.js
+++ server/test/testSample.js
@@ -1,18 +1,21 @@
 var request = require('supertest');
+var server;
 
 describe('loading express', function () {
-  var server;
   beforeEach(function () {
     server = require('../app');
   });
+
   afterEach(function () {
     server.close();
   });
+
   it('responds to /', function testSlash(done) {
-  request(server)
-    .get('/')
-    .expect(200, done);
+    request(server)
+      .get('/')
+      .expect(200, done);
   });
+
   it('404 everything else', function testPath(done) {
     request(server)
       .get('/foo/bar')
