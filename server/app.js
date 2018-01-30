3f8a121407c3dbadcf0decf1dadac526825afb5b
Anh Vo Duy <anh.voduy@harveynash.vn>
2018-01-30 12:34:12 +0700

    clean up

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
-﻿var http = require('http');
-var server = require('./server');
+﻿var server = require('./server');
 
 /* ----------- Start Server -----------*/
-http.createServer(server).listen(server.get('port'), function () {
+var app = server.listen(server.get('port'), function () {
     console.log('Node Js version:', process.version)
     console.log('eStore Web & API is running on port:' + server.get('port'));    
 });
