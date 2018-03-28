521d95cc288a8208f616daa52f12fac64857b914
Anh <anh.voduy@harveynash.vn>
2018-03-28 23:34:37 +0700

    clean up

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 ﻿var server = require('./server');
-
 var app = server.listen(server.get('port'), function () {
     console.log('Node Js version:', process.version)
     console.log('eStore Web & API is running on port: %s', app.address().port);    
