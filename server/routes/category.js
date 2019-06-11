2fe5932068559cc1899506c0280fd5c1fa15db52
devinsg <duyanh2005@gmail.com>
2019-06-11 04:32:13 +0000

    clean up & query categories

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,54 +1,12 @@
 const router = require('express').Router();
-const _ = require('lodash');
-const auth = require('../config/auth');
-const CONSTANT = require('../lib/constant');
-const dbContext = require('../lib/dbContext');
+const productService = require('../services/productService');
 
 // Router
-router.get('/items', auth.checkAuthentication(), async function (req, res, next) {
+router.get('/items', async function (req, res, next) {
 	try
 	{
-		return res.status(200).json(true);
-	}
-	catch(err){
-		next(err);
-	}
-});
-
-router.get('/item', auth.checkAuthentication(), async function (req, res, next) {
-	try
-	{		
-		return res.status(200).json(true);
-	}
-	catch(err){
-		next(err);
-	}
-});
-
-router.post('/create', auth.checkAuthentication(), async function (req, res, next) {
-	try
-	{
-		return res.status(200).json(true);
-	}
-	catch(err){
-		next(err);
-	}
-});
-
-router.post('/update', auth.checkAuthentication(), async function (req, res, next) {
-	try
-	{
-		return res.status(200).json(true);
-	}
-	catch(err){
-		next(err);
-	}		
-});
-
-router.post('/delete', auth.checkAuthentication(), async function (req, res, next) {
-	try
-	{
-		return res.status(200).json(true);
+		const categories = await productService.getCategories();
+		return res.status(200).json(categories);
 	}
 	catch(err){
 		next(err);
