9fce8e786bbdf91002c830091273ae19f6eb4435
devinsg <duyanh2005@gmail.com>
2019-06-13 04:38:55 +0000

    login by azure ad

--- server/routes/api.js
+++ server/routes/api.js
@@ -65,125 +65,125 @@ router.get('/logout1', function(req, res, next) {
 });
 
 
-passport.serializeUser(function(user, done) {
-	done(null, user.oid);
-});
+// passport.serializeUser(function(user, done) {
+// 	done(null, user.oid);
+// });
   
-passport.deserializeUser(function(oid, done) {
-	findByOid(oid, function (err, user) {
-	  	done(err, user);
-	});
-});
+// passport.deserializeUser(function(oid, done) {
+// 	findByOid(oid, function (err, user) {
+// 	  	done(err, user);
+// 	});
+// });
 
-// array to hold logged in users
-var users = [];
-var findByOid = function(oid, fn) {
-  	for (var i = 0, len = users.length; i < len; i++) {
-    	var user = users[i];
-   		console.log('we are using user: ', user);
-		if (user.oid === oid) {
-			return fn(null, user);
-		}
-  	}
-  	return fn(null, null);
-};
+// // array to hold logged in users
+// var users = [];
+// var findByOid = function(oid, fn) {
+//   	for (var i = 0, len = users.length; i < len; i++) {
+//     	var user = users[i];
+//    		console.log('we are using user: ', user);
+// 		if (user.oid === oid) {
+// 			return fn(null, user);
+// 		}
+//   	}
+//   	return fn(null, null);
+// };
 
-//-----------------------------------------------------------------------------
-// Use the OIDCStrategy within Passport.
-// 
-// Strategies in passport require a `verify` function, which accepts credentials
-// (in this case, the `oid` claim in id_token), and invoke a callback to find
-// the corresponding user object.
-// 
-// The following are the accepted prototypes for the `verify` function
-// (1) function(iss, sub, done)
-// (2) function(iss, sub, profile, done)
-// (3) function(iss, sub, profile, access_token, refresh_token, done)
-// (4) function(iss, sub, profile, access_token, refresh_token, params, done)
-// (5) function(iss, sub, profile, jwtClaims, access_token, refresh_token, params, done)
-// (6) prototype (1)-(5) with an additional `req` parameter as the first parameter
-//
-// To do prototype (6), passReqToCallback must be set to true in the config.
-//-----------------------------------------------------------------------------
-passport.use(new OIDCStrategy({
-		identityMetadata: aadConfig.creds.identityMetadata,
-		clientID: aadConfig.creds.clientID,
-		responseType: aadConfig.creds.responseType,
-		responseMode: aadConfig.creds.responseMode,
-		redirectUrl: aadConfig.creds.redirectUrl,
-		allowHttpForRedirectUrl: aadConfig.creds.allowHttpForRedirectUrl,
-		clientSecret: aadConfig.creds.clientSecret,
-		validateIssuer: aadConfig.creds.validateIssuer,
-		isB2C: aadConfig.creds.isB2C,
-		issuer: aadConfig.creds.issuer,
-		passReqToCallback: aadConfig.creds.passReqToCallback,
-		scope: aadConfig.creds.scope,
-		loggingLevel: aadConfig.creds.loggingLevel,
-		nonceLifetime: aadConfig.creds.nonceLifetime,
-		nonceMaxAmount: aadConfig.creds.nonceMaxAmount,
-		useCookieInsteadOfSession: aadConfig.creds.useCookieInsteadOfSession,
-		cookieEncryptionKeys: aadConfig.creds.cookieEncryptionKeys,
-		clockSkew: aadConfig.creds.clockSkew,
-  	},
-  	function(iss, sub, profile, accessToken, refreshToken, done) {
-		if (!profile.oid) {
-			return done(new Error("No oid found"), null);
-		}
-		// asynchronous verification, for effect...
-		process.nextTick(function () {
-			findByOid(profile.oid, function(err, user) {
-				if (err) {
-				return done(err);
-				}
-				if (!user) {
-				// "Auto-registration"
-				users.push(profile);
-				return done(null, profile);
-				}
-				return done(null, user);
-			});
-		});
-  	}
-));
+// //-----------------------------------------------------------------------------
+// // Use the OIDCStrategy within Passport.
+// // 
+// // Strategies in passport require a `verify` function, which accepts credentials
+// // (in this case, the `oid` claim in id_token), and invoke a callback to find
+// // the corresponding user object.
+// // 
+// // The following are the accepted prototypes for the `verify` function
+// // (1) function(iss, sub, done)
+// // (2) function(iss, sub, profile, done)
+// // (3) function(iss, sub, profile, access_token, refresh_token, done)
+// // (4) function(iss, sub, profile, access_token, refresh_token, params, done)
+// // (5) function(iss, sub, profile, jwtClaims, access_token, refresh_token, params, done)
+// // (6) prototype (1)-(5) with an additional `req` parameter as the first parameter
+// //
+// // To do prototype (6), passReqToCallback must be set to true in the config.
+// //-----------------------------------------------------------------------------
+// passport.use(new OIDCStrategy({
+// 		identityMetadata: aadConfig.creds.identityMetadata,
+// 		clientID: aadConfig.creds.clientID,
+// 		responseType: aadConfig.creds.responseType,
+// 		responseMode: aadConfig.creds.responseMode,
+// 		redirectUrl: aadConfig.creds.redirectUrl,
+// 		allowHttpForRedirectUrl: aadConfig.creds.allowHttpForRedirectUrl,
+// 		clientSecret: aadConfig.creds.clientSecret,
+// 		validateIssuer: aadConfig.creds.validateIssuer,
+// 		isB2C: aadConfig.creds.isB2C,
+// 		issuer: aadConfig.creds.issuer,
+// 		passReqToCallback: aadConfig.creds.passReqToCallback,
+// 		scope: aadConfig.creds.scope,
+// 		loggingLevel: aadConfig.creds.loggingLevel,
+// 		nonceLifetime: aadConfig.creds.nonceLifetime,
+// 		nonceMaxAmount: aadConfig.creds.nonceMaxAmount,
+// 		useCookieInsteadOfSession: aadConfig.creds.useCookieInsteadOfSession,
+// 		cookieEncryptionKeys: aadConfig.creds.cookieEncryptionKeys,
+// 		clockSkew: aadConfig.creds.clockSkew,
+//   	},
+//   	function(iss, sub, profile, accessToken, refreshToken, done) {
+// 		if (!profile.oid) {
+// 			return done(new Error("No oid found"), null);
+// 		}
+// 		// asynchronous verification, for effect...
+// 		process.nextTick(function () {
+// 			findByOid(profile.oid, function(err, user) {
+// 				if (err) {
+// 				return done(err);
+// 				}
+// 				if (!user) {
+// 				// "Auto-registration"
+// 				users.push(profile);
+// 				return done(null, profile);
+// 				}
+// 				return done(null, user);
+// 			});
+// 		});
+//   	}
+// ));
 
-router.post('/login', function (req, res, next) {
-	passport.authenticate('azuread-openidconnect', {
-        response: res,
-        resourceURL: aadConfig.resourceURL,
-        customState: 'my_state',
-        failureRedirect: '/'
-    })(req, res, next);
-}, function(req, res){
-	console.log('Login was called in the Sample');
-    res.redirect('/');
-});
+// router.post('/login', function (req, res, next) {
+// 	passport.authenticate('azuread-openidconnect', {
+//         response: res,
+//         resourceURL: aadConfig.resourceURL,
+//         customState: 'my_state',
+//         failureRedirect: '/'
+//     })(req, res, next);
+// }, function(req, res){
+// 	console.log('Login was called in the Sample');
+//     res.redirect('/');
+// });
 
-router.get('/auth/openid/return', function(req, res, next) {
-    passport.authenticate('azuread-openidconnect', {
-		response: res,
-		failureRedirect: '/'  
-    })(req, res, next);
-}, function(req, res) {
-    console.log('We received a return from AzureAD.');
-	res.redirect('/');
-});
+// router.get('/auth/openid/return', function(req, res, next) {
+//     passport.authenticate('azuread-openidconnect', {
+// 		response: res,
+// 		failureRedirect: '/'  
+//     })(req, res, next);
+// }, function(req, res) {
+//     console.log('We received a return from AzureAD.');
+// 	res.redirect('/');
+// });
 
-router.post('/auth/openid/return', function(req, res, next) {
-    passport.authenticate('azuread-openidconnect', {
-		response: res,
-        failureRedirect: '/'
-    })(req, res, next);
-}, function(req, res) {
-	console.log('We received a return from AzureAD.');
-    res.redirect('/');
-});
+// router.post('/auth/openid/return', function(req, res, next) {
+//     passport.authenticate('azuread-openidconnect', {
+// 		response: res,
+//         failureRedirect: '/'
+//     })(req, res, next);
+// }, function(req, res) {
+// 	console.log('We received a return from AzureAD.');
+//     res.redirect('/');
+// });
 
-router.get('/logout', function(req, res) {
-	req.session.destroy(function(err) {
-		req.logOut();
-		res.redirect(aadConfig.destroySessionUrl);
-	});
-});
+// router.get('/logout', function(req, res) {
+// 	req.session.destroy(function(err) {
+// 		req.logOut();
+// 		res.redirect(aadConfig.destroySessionUrl);
+// 	});
+// });
 
 router.post('/upload', auth.checkAuthentication(), uploadProductImageFS, async function(req, res, next){
 	try
